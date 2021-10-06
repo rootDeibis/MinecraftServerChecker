688ef1a6da71ba4eb7c228fab887b32409bff580
Deibis Alejandro <35423510+rootDeibis@users.noreply.github.com>
2021-10-06 02:55:24 -1200

    Update MinecraftServerCheck.js
    Rewritten code and added a change detector on specific values.

--- src/MinecraftServerCheck.js
+++ src/MinecraftServerCheck.js
@@ -3,42 +3,82 @@ This script was created by OA
 Twitter: OA42236246
 */
 
+/* EXAMPLE DETECT CHANGES 
+
+	getServerStatus("mc.hypixel.net", "players.online", "motd.clean.0", "motd.clean.1")
+*/
+
+
+const __SERVER_STATUS_API_URL = "https://api.mcsrvstat.us/2/";
+const __UPDATE_TIME = 6; // SECONDS
+
+var __INTERVAL_ = null;
+var __OLDER_STATUS_DATA = null;
+var __NEW_STATUS_DATA = {};
 
 
 
-const ServerStatus = function(andress, port) {
-	var Global = {};
-		Global.variables = {},
-		Global.variables.api_url = "https://api.mcsrvstat.us/2/",
-		Global.variables.server_resolved = undefined;
 
 
-	if (andress == undefined) return console.error("[ServerStatus] Enter Server Andress.");
-
-	Global.variables.api_url += andress;
-
-	if (port) Global.variables.api_url += ":" + port;
-
-
-	Global.functions = {};
-
-	Global.functions.getData =  function(callback) {
-		fetch(Global.variables.api_url).then(r => {
-			r.json().then(d => {
-				Global.variables.server_resolved = d;
-				if(callback != undefined && typeof callback === 'function') {
-					callback(d);
-
-				} else {
-					console.log('[ServerStatus] Server Checked');
-				}
-
-			})
-		});
-		return Global.variables;
-	}
-
-
-
-	return Global.functions;
+var ServerStatusOnPlayerChangeEvent = function(data = {}) {
+	console.log('There have been changes in the server data.', data);
 }
+
+function getServerStatus(andress, ...__DETECT_CHANGES) {
+	if (andress == undefined) return {messsage: 'Unknown andress'};
+
+	return new Promise(async(resolve, reject) => {
+		try {
+			var RequestURL = await fetch(__SERVER_STATUS_API_URL + andress);
+			var Response = await RequestURL.json();
+
+			if (!Response.online) return reject(Response);
+
+			if (__INTERVAL_ == null) {
+				__INTERVAL_ = setInterval(async() => {
+					getServerStatus(andress).then((data) => {
+					let changed = false;
+					for(var find of __DETECT_CHANGES){
+						var findKeys = find.split(".");
+
+						var currentValue = data;
+						var olderValue = __NEW_STATUS_DATA;
+
+						for(var key of findKeys) {
+							currentValue = currentValue[key];
+							olderValue = olderValue[key];
+						}
+						
+
+						if (currentValue != olderValue && currentValue == null && olderValue == null) {
+							changed = true;
+							break;
+						}
+					}
+
+
+					if (changed) {
+						__OLDER_STATUS_DATA = __NEW_STATUS_DATA;
+						__NEW_STATUS_DATA = data;
+
+						ServerStatusOnPlayerChangeEvent(data);
+					}
+						
+						
+					}).catch(reject);
+
+				}, __UPDATE_TIME * 1000);
+			}
+
+			resolve(Response);
+
+			__NEW_STATUS_DATA = Response;
+			if (__OLDER_STATUS_DATA == null) __OLDER_STATUS_DATA = Response;
+		} catch(e) {
+			reject(e);
+		}
+	});
+}
+
+
+
